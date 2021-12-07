d124778d6d7cb7fc9523c6ab1e94e39118fec065
Ömer Esin <omeresinyildiz@gmail.com>
2021-12-07 19:30:49 +0100

    First Commit

---
+++ ConsoleNet5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleNet5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("İsminizi girin:");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Soyisminizi girin:");
+            string lastName = Console.ReadLine();
+
+            string fullName = String.Concat(firstName, " ", lastName);
+            Console.WriteLine(fullName);
+            Console.WriteLine($"İsminizi: {firstName} ve soyisminiz: {lastName}");
+        }
+    }
+}
