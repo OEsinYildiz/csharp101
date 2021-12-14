6bb79236def429ece3ce5ed96713074317c1ff6e
Ömer Esin <omeresinyildiz@gmail.com>
2021-12-14 20:34:02 +0100

    Try-Catch

---
+++ PatikaExercises/Try_Catch/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+
+try
+{
+    Console.WriteLine("Sayı Gir");
+    int sayi = Convert.ToInt32(Decimal.MaxValue);
+    Console.WriteLine(sayi);
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine(ex);
+    Console.WriteLine("Argument");
+
+}
+catch (FormatException ex)
+{
+    Console.WriteLine(ex);
+    Console.WriteLine("Format");
+}
+catch (OverflowException ex)
+{
+    Console.WriteLine(ex);
+    Console.WriteLine("Overflow");
+}
+finally
+{
+    Console.WriteLine("İşlem Tamam!");
+}
